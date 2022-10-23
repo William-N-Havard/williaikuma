d6bbe3d2da615066e1389d50c993f0ce30bb7d9f
William N. Havard <william.havard@gmail.com>
2022-10-24 00:08:22 +0200

    Added faulthandler

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!usr/bin/env python
 # -*- coding: utf8 -*-
 
+import sys
+import faulthandler
+
 from App import App
 
 if __name__ == "__main__":
+    faulthandler.enable(file=sys.stderr, all_threads=True)
+
     app = App()
     app.mainloop()
--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 eval "$(conda shell.bash hook)"
 conda activate RECORDER
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/alsa-lib/
+export PYTHONFAULTHANDLER=1
 python main.py
